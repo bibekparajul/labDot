471ca5fb11b2cdfe218cd61d038c30f2c119fa15
Bibek Parajuli <parajulibbek11@gmail.com>
2023-03-30 07:55:53 +0545

    excHandling

--- DemoAppCsit/Program.cs
+++ DemoAppCsit/Program.cs
@@ -7,10 +7,31 @@ namespace DemoAppCsit {
         public static void Main(string[] args)
         {
 
-            MyClass c = new MyClass();
-            c.Display1();
-            c.Display2();
-           
+            try
+            {
+                int a = 1;
+                int b = 0;
+                if (b == 0)
+                {
+                    throw new Exception("Cannot divide by 0");
+                }
+                int c = a / b;
+                Console.WriteLine("Completed Try Block");
+
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch
+            {
+                Console.WriteLine("Generic exception block");
+            }
+            finally
+            {
+                Console.WriteLine("this is finally block");
+            }
+
 
         }
     }
