b52699ef11eb60692e59550c650b65c9ac739b54
Bibek Parajuli <parajulibbek11@gmail.com>
2023-03-31 17:48:53 +0545

    positionalArgs

--- DemoAppCsit/Program.cs
+++ DemoAppCsit/Program.cs
@@ -7,32 +7,43 @@ namespace DemoAppCsit {
         public static void Main(string[] args)
         {
 
-            try
-            {
-                int a = 1;
-                int b = 0;
-                if (b == 0)
-                {
-                    throw new Exception("Cannot divide by 0");
-                }
-                int c = a / b;
-                Console.WriteLine("Completed Try Block");
+            //try
+            //{
+            //    int a = 1;
+            //    int b = 0;
+            //    if (b == 0)
+            //    {
+            //        throw new Exception("Cannot divide by 0");
+            //    }
+            //    int c = a / b;
+            //    Console.WriteLine("Completed Try Block");
 
-            }
-            catch(DivideByZeroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch
-            {
-                Console.WriteLine("Generic exception block");
-            }
-            finally
-            {
-                Console.WriteLine("this is finally block");
-            }
+            //}
+            //catch(DivideByZeroException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("Generic exception block");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("this is finally block");
+            //}
+            DisplayInfo(2,"Bbek","Nepali");        //positional args
 
+            DisplayInfo(name: "Dyan", age: 22, nationality: "USA"); //named ards
 
+            //optional args if muni ko mai default value dini ...
+        
+        }
+
+        public static void DisplayInfo(int age , string name, string nationality)  //string nationality = "Nepal"
+        {
+            Console.WriteLine("Age:" + age);
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("Nationality:" + nationality);
         }
     }
 
